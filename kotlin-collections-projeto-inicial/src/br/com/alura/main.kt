c920e9e11ec20fa21fe77cde84943caa693d8363
Borges <paulo.borges@premiersoft.net>
2022-01-20 09:09:18 -0300

    introduction to kotlin course 07

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -1,40 +1,63 @@
 package br.com.alura
 
 fun main() {
-    val myFunctionLambda : () -> Unit = {
-        println("execute as lambda")
+//
+//    testFunctionClass()
+//    testFunctionReference()
+
+
+    val myFunctionLambda: (Int, Int) -> Int = { a, b ->
+        a + b
     }
 
-    println(myFunctionLambda())
+    println(myFunctionLambda(15, 10))
 
-    val anonymousFunction : () -> Unit = fun () { println("execute as anonymous")}
-    println(anonymousFunction())
+    val anonymousFunction: (Int, Int) -> Int = fun(a, b): Int {
+        println("execute as anonymous")
+        return a + b
+    }
+    println(anonymousFunction(20, 10))
+
+    val calculateBonus: (salary: Double) -> Double = lambda@{salary ->
+
+        if (salary > 1000.0) {
+            return@lambda salary + 50
+        }
+        salary + 100.0
+    }
+
+    println(calculateBonus(1100.0))
+
+    val calculateBonusAnonymous : (salary: Double ) -> Double = fun(salary): Double{
+        if(salary > 1000.0) return salary + 50
+        return salary + 100.0
+    }
+
+    println(calculateBonusAnonymous(1100.0))
 }
-
 
 fun testFunctionClass() {
 
-    val myFunctionClass: () -> Unit = Test()
-    println(myFunctionClass())
+    val myFunctionClass: (Int, Int) -> Int = PlusSum()
+    println(myFunctionClass(10, 10))
 
 }
 
 fun testFunctionReference() {
 
-    val myFunction: () -> Unit = ::test
+//    val myFunction: () -> Unit = ::plusSum
+    val myFunction: (Int, Int) -> Int = ::plusSum
 
-    println(myFunction())
+    println(myFunction(5, 10))
 }
 
-fun test() {
-    println("execute test")
-
+fun plusSum(a: Int, b: Int): Int {
+    return a + b
 }
 
-class Test : () -> Unit {
-
-    override fun invoke() {
-        println("execute test invoke")
+class PlusSum : (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int {
+        return p1 + p2
     }
 }
 
